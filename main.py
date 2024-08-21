ff9802edf7a49e9f7841c4fe7647f3bc1f5a9c51
YOSRI BEN HALIMA <yosri.benhalima@ept.ucar.tn>
2024-08-21 16:03:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -116,7 +116,13 @@ def plot_data(tickerDf, sp500Df, data_options, ohlc_option, compare_to_benchmark
                                              low=sp500Df['Low'],
                                              close=sp500Df['Close'],
                                              name='^GSPC'))
-            fig.update_layout(xaxis_title='Date', yaxis_title='Price ($)', xaxis_rangeslider_visible=False, template='plotly_dark')
+            fig.update_layout(xaxis_title='Date',
+                              yaxis_title='Price ($)',
+                              xaxis_rangeslider_visible=False,
+                              template='plotly_dark',
+                              hovermode='x unified',
+                              legend=dict(orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1),
+                              margin=dict(l=40, r=40, t=40, b=40))
             st.plotly_chart(fig)
         else:
             st.subheader("OHLC")
@@ -127,7 +133,12 @@ def plot_data(tickerDf, sp500Df, data_options, ohlc_option, compare_to_benchmark
             fig.add_trace(go.Scatter(x=tickerDf['Date'], y=tickerDf['Close'], mode='lines', name=f'{tickerSymbol} Close', line=dict(color='orange')))
             if compare_to_benchmark:
                 fig.add_trace(go.Scatter(x=sp500Df['Date'], y=sp500Df['Close'], mode='lines', name='S&P 500 Close', line=dict(color='#FAFAFA')))
-            fig.update_layout(xaxis_title='Date', yaxis_title='Price', template='plotly_dark')
+            fig.update_layout(xaxis_title='Date',
+                              yaxis_title='Price',
+                              template='plotly_dark',
+                              hovermode='x unified',
+                              legend=dict(orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1),
+                              margin=dict(l=40, r=40, t=40, b=40))
             st.plotly_chart(fig)
 
     elif data_options == 'Returns & Performance':
@@ -144,7 +155,12 @@ def plot_data(tickerDf, sp500Df, data_options, ohlc_option, compare_to_benchmark
                 if compare_to_benchmark:
                     if metric in sp500Df.columns:
                         fig.add_trace(go.Scatter(x=sp500Df['Date'], y=sp500Df[metric], mode='lines', name='^GSPC', line=dict(color='#FAFAFA')))
-                fig.update_layout(xaxis_title='Date', yaxis_title=f'{metric}', template='plotly_dark')
+                fig.update_layout(xaxis_title='Date',
+                                  yaxis_title=f'{metric}',
+                                  template='plotly_dark',
+                                 hovermode='x unified',
+                                 legend=dict(orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1),
+                                 margin=dict(l=40, r=40, t=40, b=40))
                 st.plotly_chart(fig)
 
         st.subheader("Returns Distribution")
@@ -152,7 +168,13 @@ def plot_data(tickerDf, sp500Df, data_options, ohlc_option, compare_to_benchmark
         fig.add_trace(go.Histogram(x=tickerDf['Stock Returns'], name=tickerSymbol, marker=dict(color='#4bffb0'), opacity=0.7, nbinsx=50))
         if compare_to_benchmark:
             fig.add_trace(go.Histogram(x=sp500Df['Stock Returns'], name='^GSPC', marker=dict(color='#FAFAFA'), opacity=0.7, nbinsx=50))
-        fig.update_layout(xaxis_title='Returns', yaxis_title='Frequency', barmode='overlay', template='plotly_dark')
+        fig.update_layout(xaxis_title='Returns', 
+                          yaxis_title='Frequency', 
+                          barmode='overlay', 
+                          template='plotly_dark',
+                          hovermode='x unified',
+                          legend=dict(orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1),
+                          margin=dict(l=40, r=40, t=40, b=40))
         st.plotly_chart(fig)
 
     elif data_options == 'Additional Information':
